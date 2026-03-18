ac8fd4cb3ca941eb4865f0ac06ea18865d7fe36d
Kim-buyeon <bu2000kim@gmail.com>
2026-03-18 20:37:51 +0900

    feat: 블럭 생성, 이동, 기준선 체크 하는 로직 구현(#19)

--- g_line/js/g_line.js
+++ g_line/js/g_line.js
@@ -1,25 +1,30 @@
 const guide = document.getElementById("line-guide")
-guide.innerText = "움직이는 그래프가 랜덤 기준선에 닿는 순간 버튼을 누르는 타이밍 게임"
+guide.innerText = "움직이는 블럭이 랜덤 기준선에 닿는 순간 버튼을 누르는 타이밍 게임"
 const startButton = document.getElementById("startButton")
 startButton.innerText = "게임 시작하기"
 const body = document.getElementsByTagName('body')
 const container = document.getElementById('container')
-
+let animationId = null
+let blockY = null
 let gamestate = "HOME"
+let isSuccess= false
 //버튼의 상태 정보를 업데이트 하는 함수
 function setGameState(newState){
     gamestate = newState
     render()
 }
 
-//버튼을 클릭 할 때마다 버튼의 스타일을 변경하기 위해 존재하는 함수
+//버튼을 클릭 할 때마다 수행햐야 할 로직 수행 및 버튼 스타일 변경하는 함수
 function render(){
     
     const oldMsg = document.getElementById("result-msg")
     const oldLine = document.querySelectorAll(".line")
+    const oldBlock = document.getElementById("block")
     if(oldLine){
         oldLine.forEach((li)=>li.remove())
     }
+    if(oldBlock) oldBlock.remove() 
+    if(animationId) cancelAnimationFrame(animationId)
 
     if(oldMsg)oldMsg.remove()
     if(gamestate == "HOME"){
@@ -28,30 +33,39 @@ function render(){
         startButton.onclick = () => setGameState("PLAYING")
     }else if(gamestate == 'PLAYING'){
         drawLine()
+        generateBlock();
+        blockY = 80
+        moveBlock()
         container.style.backgroundImage = "none";
         startButton.style.backgroundColor = "#ef4444"
         startButton.innerText = "타이밍 맞춰 누르세요"
-        startButton.onclick = () => setGameState("RESULT");
+        startButton.onclick = () =>{
+            isSuccess = checkBaseLine();
+            setGameState("RESULT");
+        } 
     }else if(gamestate == "RESULT"){
         container.style.backgroundImage = "url('../../asset/img/linegamecharacter.png')";
         const p = document.createElement("p")
         p.id = "result-msg";
+        if(isSuccess){
+            p.innerText = "스테이지에 성공하셨습니다"
+            p.style.color = "black"
+        }else{
+            p.innerText = "실패했습니다."
+            p.style.color = "#ef4444"
+        }
         p.style.fontSize = "40px"
         p.style.fontWeight = "bold"
-        p.style.color = "black"
         p.style.textAlign = "center";
         p.style.position = 'absolute'
         p.style.left = "50%"
         p.style.top = "70%"
         p.style.whiteSpace = "nowrap";
         p.style.transform = "translate(-50%,-50%)"
-        //성공 여부에 따라  텍스트 내용 바뀜
-        p.innerText = "스테이지 성공하셨습니다."
-        startButton.innerText = "게임시작하기"
         startButton.style.backgroundColor = "#263747";
+        startButton.innerText = "게임시작하기"
         startButton.before(p);   
         startButton.onclick = () => setGameState("HOME")
-        startButton.onclick = () => setGameState("HOME");
     }
 }
 
@@ -67,7 +81,7 @@ function drawLine() {
     const buttonTop = startButton.offsetTop;
 
     let min = guideBottom + 20;
-    let max = buttonTop - 50; 
+    let max = buttonTop - 70; 
 
     if (max < min) max = min + 10;
 
@@ -75,16 +89,69 @@ function drawLine() {
 
     const line = document.createElement('div');
     line.className = 'line';
-    line.style.borderTop = "2px solid black";
+    line.style.borderTop = "3px solid gold";
     line.style.position = 'absolute';
     line.style.left = "50%";
     line.style.top = `${y}px`;
     line.style.width = "100%";
     line.style.transform = "translate(-50%, -50%)";
-    line.style.zIndex = "5";
+    //line.style.zIndex = "5";
     
     container.appendChild(line);
 }
+//블럭 생성하는 함수
+function generateBlock(){
+    const block = document.createElement("div");
+    block.id = "block";
+    block.style.position = "absolute";
+    block.style.left = "50%";
+    block.style.top = "70%"
+    block.style.transform = "translate(-50%,-50%)"
+    block.style.backgroundColor = "#2ecc71"
+    block.style.width = "5%"
+    block.style.height = "5%"
+    block.style.boxShadow = "0 0 10px #2ecc71,0 0 20px #2ecc71,0 0 40px #27ae60";
+    block.style.filter = "brightness(1.2)";
+    startButton.before(block)
+}
+
+let baseSpeed  = 0.5
+//블럭 이동하는 함수
+function moveBlock(){
+    let varialeSpeed = baseSpeed + (Math.random() * 3.4);
+    blockY -= varialeSpeed
+    const containerHeight = container.offsetHeight;
+    const guideLimit = ((guide.offsetTop + guide.offsetHeight) / containerHeight) * 100;
+
+    //가이드에 닿았는지 확인
+    if (blockY <= guideLimit + 2.5) {
+        blockY = 80; 
+    }
+    const block = document.getElementById("block");
+    if (block) {
+        block.style.top = blockY + "%";
+        
+        block.style.filter = `brightness(${1.2 + Math.random() * 0.3})`;
+    }
+    animationId = requestAnimationFrame(moveBlock); 
+}
+
+//기준선에 도달했는지 확인하는 함수
+function checkBaseLine(){
+    const baseLine = document.querySelector('.line');
+    const block = document.getElementById("block");
+    if(!baseLine || !block)return;
+    const targetY = baseLine.offsetTop;
+    const currentY = block.offsetTop;
+    const tolerance = 25;
+    const diff = Math.abs(targetY - currentY);
+    if (diff <=tolerance){
+        return true;
+    }else{
+        return false
+    }
+}
+
 render();
 
 
